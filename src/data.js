505bc3ca939bd715703ad4e8b1d8d94beab5bc44
Ricardo Momoi <shinjimomoi@gmail.com>
2022-06-15 19:21:35 +0900

    Made the travel journal

--- src/data.js
+++ src/data.js
@@ -1,30 +1,57 @@
 const data = [
-    {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    },
-    {
-        title: "Hakone",
+  {
+      title: "Bangkok",
+      location: "Thailand",
+      googleMapsUrl: "https://goo.gl/maps/iB2Tahgubn6gUsk16",
+      startDate: "12 Jan, 2020",
+      endDate: "18 Jan, 2020",
+      description: "There are cities and then there is Bangkok. Temples like Wat Pho are thronged with devotees all day. Street food sizzles all evening. The heat never stops. And the nightlife—from sleazy Patpong to ritzy rooftop bars—is second to none. But the pleasures don’t end there. Night markets, mega malls, and the massive Chatuchak Weekend Market all make Bangkok a shoppers’ (and souvenir-hunter’s) dream.",
+      imageUrl: "https://source.unsplash.com/BvNNxnzds4U"
+  },
+  {
+    title: "Hakone",
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/Lw8Pga9EjQfzK38DA",
         startDate: "18 Jun, 2022",
         endDate: "24 Jan, 2022",
         description: "Hakone is close to Tokyo and Mount Fuji and is a popular travel destination. Hakone can be enjoyed year-round with its stunning seasonal landscape and many events. Located between Tokyo and western Japan, you can take the Shinkansen to Odawara and go to Osaka and Kyoto via Hakone.",
         imageUrl: "https://source.unsplash.com/fpkbCzqSP5Y"
-    },
-    {
+      },
+      {
         title: "Osaka",
         location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/Lw8Pga9EjQfzK38DA",
+        googleMapsUrl: "https://goo.gl/maps/rHjC2GeJ39Z48QUH9",
         startDate: "02 Feb, 2022",
         endDate: "10 Feb, 2022",
         description: "Osaka (大阪, Ōsaka) is Japan's second largest metropolitan area after Tokyo. It has been the economic powerhouse of the Kansai Region for many centuries. Before the Nara Period, when the capital used to be moved with the reign of each new emperor, Osaka was once Japan's capital city, the first one ever known.",
         imageUrl: "https://source.unsplash.com/9zYjlA4LqTU"
+      },
+      {
+            title: "Mount Fuji",
+            location: "Japan",
+            googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+            startDate: "12 Jan, 2021",
+            endDate: "24 Jan, 2021",
+            description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+            imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        },
+    {
+        title: "Nikko",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/YBVS7ZQyanqPGZNk7",
+        startDate: "04 Jul, 202",
+        endDate: "14 Jul, 2020",
+        description: "Nikko had been a center of Shinto and Buddhist mountain worship for many centuries before Toshogu was built in the 1600s, and Nikko National Park continues to offer scenic, mountainous landscapes, lakes, waterfalls, hot springs, wild monkeys and hiking trails.",
+        imageUrl: "https://source.unsplash.com/KrUZXoQPhNg"
+    },
+    {
+        title: "Seoul",
+        location: "South Korea",
+        googleMapsUrl: "https://goo.gl/maps/EXhguULmwsuokta77",
+        startDate: "12 Jan, 2020",
+        endDate: "18 Jan, 2020",
+        description: "From modern skyscrapers and neon lights to Buddhist temples, palaces and pagodas, Seoul is a fascinating mix of old and new. Take it all in from the N Seoul Tower, built atop a peak in Namsan Park.",
+        imageUrl: "https://source.unsplash.com/Y2JQGXUM1iU"
     },
 ]
 
