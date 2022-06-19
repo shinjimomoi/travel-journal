ea5c018d32d10dc09fd5d1724dc254ea7e7e13a4
Ricardo Momoi <shinjimomoi@gmail.com>
2022-06-19 14:39:19 +0900

    Added more data

--- src/data.js
+++ src/data.js
@@ -9,6 +9,33 @@ const data = [
       imageUrl: "https://source.unsplash.com/BvNNxnzds4U"
   },
   {
+      title: "Bangkok",
+      location: "Thailand",
+      googleMapsUrl: "https://goo.gl/maps/iB2Tahgubn6gUsk16",
+      startDate: "12 Jan, 2020",
+      endDate: "18 Jan, 2020",
+      description: "There are cities and then there is Bangkok. Temples like Wat Pho are thronged with devotees all day. Street food sizzles all evening. The heat never stops. And the nightlife—from sleazy Patpong to ritzy rooftop bars—is second to none. But the pleasures don’t end there. Night markets, mega malls, and the massive Chatuchak Weekend Market all make Bangkok a shoppers’ (and souvenir-hunter’s) dream.",
+      imageUrl: "https://source.unsplash.com/BvNNxnzds4U"
+  },
+  {
+      title: "Bangkok",
+      location: "Thailand",
+      googleMapsUrl: "https://goo.gl/maps/iB2Tahgubn6gUsk16",
+      startDate: "12 Jan, 2020",
+      endDate: "18 Jan, 2020",
+      description: "There are cities and then there is Bangkok. Temples like Wat Pho are thronged with devotees all day. Street food sizzles all evening. The heat never stops. And the nightlife—from sleazy Patpong to ritzy rooftop bars—is second to none. But the pleasures don’t end there. Night markets, mega malls, and the massive Chatuchak Weekend Market all make Bangkok a shoppers’ (and souvenir-hunter’s) dream.",
+      imageUrl: "https://source.unsplash.com/BvNNxnzds4U"
+  },
+  {
+      title: "Bangkok",
+      location: "Thailand",
+      googleMapsUrl: "https://goo.gl/maps/iB2Tahgubn6gUsk16",
+      startDate: "12 Jan, 2020",
+      endDate: "18 Jan, 2020",
+      description: "There are cities and then there is Bangkok. Temples like Wat Pho are thronged with devotees all day. Street food sizzles all evening. The heat never stops. And the nightlife—from sleazy Patpong to ritzy rooftop bars—is second to none. But the pleasures don’t end there. Night markets, mega malls, and the massive Chatuchak Weekend Market all make Bangkok a shoppers’ (and souvenir-hunter’s) dream.",
+      imageUrl: "https://source.unsplash.com/BvNNxnzds4U"
+  },
+  {
     title: "Hakone",
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/Lw8Pga9EjQfzK38DA",
@@ -27,13 +54,13 @@ const data = [
         imageUrl: "https://source.unsplash.com/9zYjlA4LqTU"
       },
       {
-            title: "Mount Fuji",
-            location: "Japan",
-            googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-            startDate: "12 Jan, 2021",
-            endDate: "24 Jan, 2021",
-            description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-            imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
         },
     {
         title: "Nikko",
@@ -47,6 +74,15 @@ const data = [
     {
         title: "Seoul",
         location: "South Korea",
+        googleMapsUrl: "https://goo.gl/maps/EXhguULmwsuokta77",
+        startDate: "12 Jan, 2020",
+        endDate: "18 Jan, 2020",
+        description: "From modern skyscrapers and neon lights to Buddhist temples, palaces and pagodas, Seoul is a fascinating mix of old and new. Take it all in from the N Seoul Tower, built atop a peak in Namsan Park.",
+        imageUrl: "https://source.unsplash.com/Y2JQGXUM1iU"
+    },
+    {
+            title: "Ohio",
+        location: "USA",
         googleMapsUrl: "https://goo.gl/maps/EXhguULmwsuokta77",
         startDate: "12 Jan, 2020",
         endDate: "18 Jan, 2020",
